848b9909523b222f6501522bd0db2c65c153c23d
Eugene HP <eugene.hp2012@gmail.com>
2014-06-22 02:57:02 +0200

    fixed Xcode 6 for iOS 8.0 and OS X 10.10

--- EZAudio/EZAudioPlotGLKViewController.h
+++ EZAudio/EZAudioPlotGLKViewController.h
@@ -23,6 +23,7 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //  THE SOFTWARE.
 
+#import <Foundation/Foundation.h>
 #import "TargetConditionals.h"
 
 #if TARGET_OS_IPHONE
--- EZAudio/EZAudioPlotGLKViewController.m
+++ EZAudio/EZAudioPlotGLKViewController.m
@@ -23,6 +23,8 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //  THE SOFTWARE.
 
+#import <Foundation/Foundation.h>
+
 #if TARGET_OS_IPHONE
 
 #import "EZAudioPlotGLKViewController.h"
